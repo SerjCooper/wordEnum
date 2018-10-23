1ecacd38dca94d2dfb779c43b6d67508aead65f4
Serj Cooper <b-sergi@mail.ru>
2018-10-23 19:35:43 +0500

    Привёл в рабочий вид и коментов

--- src/main/java/EnumWords.java
+++ src/main/java/EnumWords.java
@@ -1,41 +1,87 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.Arrays;
 
 import com.google.gson.*;
 
 
-public class EnumWords{
 
-    private String filePath;
+public class EnumWords {
+
+    private String message; //сообщение которое будет обрабатываться
+    private int wordCount; // счётчик искомого слова
+    private int simbolCount; //счётчик знаков препинания
 
 
-    public String getFilePath() {
-        return filePath;
+    public int getWordCount() {
+        return wordCount;
     }
 
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
+    public void setWordCount(int wordCount) {
+        this.wordCount = wordCount;
     }
-    private static void readJson(String filepath) {
+
+    public int getSimbolCount() {
+        return simbolCount;
+    }
+
+    public void setSimbolCount(int simbolCount) {
+        this.simbolCount = simbolCount;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void readJson(String filepath) { //метод для чтения json
         JsonParser parser = new JsonParser();
+        String elements[] = new String[10];
+        int i = 0;
         try {
             JsonArray jArray = (JsonArray) parser.parse(new FileReader(filepath));
-
-            for (Object o : jArray){
-                    JsonObject rootObject = (JsonObject) o;
-
-                    String message = rootObject.get("message").getAsString(); // get property "message"
-                    JsonObject childObject = rootObject.getAsJsonObject("place"); // get place object
-                    String place = childObject.get("name").getAsString(); // get property "name"
-                    System.out.println(message + " " + place); // print "Hi World!"*/
-                }
-            }catch (FileNotFoundException e){
+            for (Object o : jArray) {
+                JsonObject root = (JsonObject) o;
+                elements[i] = root.get("greeting").getAsString();//условимся, что именно это поле нас интересует
+                i++;
+            }
+            setMessage(Arrays.toString(elements)); //пишем в поле message полученную строку из всего содержимого полей greeting
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void searchWord(String message, String targetWord) { //ищем и считаем требуемое слово
+        System.out.println(message);
+        message = message.toLowerCase();
+        targetWord = targetWord.toLowerCase(); //приводим обе строки к одному регистру
+        int i = message.indexOf(targetWord);
+        int count = 0;
+        while (i >= 0) {
+            count++;
+            i = message.indexOf(targetWord, i + 1);
+        }
+        setWordCount(count);
+    }
+
+    public void searchSimbols(String message) { //ищем все знаки препинания
+        char[] c = {'.', ',', '-', ':', ';', '?', '!'};
+        int count = 0;
+
+        char[] ch = message.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            for (int j = 0; j < c.length; j++) {
+                if (ch[i] == c[j]) {
+                    count++;
+                }
+                setSimbolCount(count);
+            }
+
+
         }
 
     }
-
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        EnumWords ew = new EnumWords();
+        EnumWords ew = new EnumWords();     //создаем экземпляр класса для поиска слова и знаков препинания
         Scanner input = new Scanner(System.in);
-        String path = "generated.json";
 
+        System.out.println("Введите искомое слово: ");
+        String wordTarget = input.nextLine();
 
+        String filepath = "./src/main/resources/generated.json";
 
+        ew.readJson(filepath);
+        System.out.println("Читаем файл...");
+        ew.searchWord(ew.getMessage(), wordTarget);
+        System.out.println("Слово " + wordTarget + " повторяется " + ew.getWordCount() + " раз");
 
+        ew.searchSimbols(ew.getMessage());
+        System.out.println("Количество знаков препинания в файле: " + ew.getSimbolCount());
     }
 }
